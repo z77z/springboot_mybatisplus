f3eedb6d910f0729c43394c1936488c1c15060f7
z77z <1093615728@qq.com>
2017-02-17 13:27:59 +0800

    init

--- src/main/java/io/z77z/config/ShiroConfig.java
+++ src/main/java/io/z77z/config/ShiroConfig.java
@@ -10,12 +10,7 @@ import java.util.Map;
 
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.crazycake.shiro.RedisCacheManager;
-import org.crazycake.shiro.RedisManager;
-import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.apache.shiro.mgt.SecurityManager;
@@ -30,12 +25,6 @@ public class ShiroConfig {
 
 	@Autowired
 	SysPermissionInitService sysPermissionInitService;
-	
-	@Value("${spring.redis.host}")
-	private String host;
-	
-	@Value("${spring.redis.port}")
-	private int port;
 
 	/**
 	 * ShiroFilterFactoryBean 处理拦截资源文件问题。
@@ -87,10 +76,6 @@ public class ShiroConfig {
 		DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
 		// 设置realm.
 		securityManager.setRealm(myShiroRealm());
-		//自定义缓存实现  使用redis
-		securityManager.setCacheManager(cacheManager());
-		//自定义session管理 使用redis
-		securityManager.setSessionManager(SessionManager());
 		return securityManager;
 	}
 
@@ -99,51 +84,9 @@ public class ShiroConfig {
 	 * 
 	 * @return
 	 */
+	@Bean
 	public MyShiroRealm myShiroRealm() {
 		MyShiroRealm myShiroRealm = new MyShiroRealm();
 		return myShiroRealm;
 	}
-	
-	/**
-	 * 配置shiro redisManager
-	 * @return
-	 */
-	public RedisManager redisManager(){
-		RedisManager redisManager = new RedisManager();
-		redisManager.setHost(host);
-		redisManager.setPort(port);
-		redisManager.setExpire(1800);//配置过期时间
-		//redisManager.setTimeout(timeout);
-		//redisManager.setPassword(password);
-		return redisManager;
-	}
-	
-	/**
-	 * cacheManager  缓存 redis实现
-	 * @return
-	 */
-	public RedisCacheManager cacheManager(){
-		RedisCacheManager redisCacheManager = new RedisCacheManager();
-		redisCacheManager.setRedisManager(redisManager());
-		return redisCacheManager;
-	}
-	
-	/**
-	 * RedisSessionDAO shiro sessionDao层的实现 通过redis
-	 */
-	public RedisSessionDAO redisSessionDAO(){
-		RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
-		redisSessionDAO.setRedisManager(redisManager());
-		return redisSessionDAO;
-	}
-	
-	/**
-	 * shiro session的管理
-	 */
-	public DefaultWebSessionManager SessionManager(){
-		DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-		sessionManager.setSessionDAO(redisSessionDAO());
-		return sessionManager;
-	}
-	
 }
